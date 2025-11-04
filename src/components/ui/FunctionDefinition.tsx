1162d7c114c6d8d2aa20ab929ce89808bb17e921
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2025-11-04 12:10:05 +0900

    refactor: inline function name decoration logic into function definition component

--- src/components/ui/FunctionDefinition.tsx
+++ src/components/ui/FunctionDefinition.tsx
@@ -2,9 +2,24 @@ import type { FC } from "hono/jsx";
 import { basePath } from "../../metadata";
 import type { Func } from "../../types/model";
 import { joinPath } from "../../utils/path";
-import { genPath } from "./genPath";
 import { TypeIcon } from "./TypeIcon";
 import { buildParamId, type2href } from "./type2href";
+
+/**
+ * Returns the decorated function name with `.typ-func` and `.typ-punct` spans.
+ *
+ * For Typst v0.14.0, the path is either empty or has only one item.
+ */
+const DecoratedFuncName: FC<{ func: Func }> = ({ func }) => {
+	const funcName = func.self ? "self" : func.path.join(".");
+	return (
+		<>
+			<span>{funcName}</span>
+			{funcName !== "" && <span class="typ-punct">.</span>}
+			<span class="typ-func">{func.name}</span>
+		</>
+	);
+};
 
 type FunctionDefinitionProps = {
 	func: Func;
@@ -20,13 +35,10 @@ export const FunctionDefinition: FC<FunctionDefinitionProps> = ({
 	func,
 	prefix = undefined,
 }) => {
-	const funcName = func.self ? "self" : genPath(func);
 	return (
 		<pre>
 			<code>
-				<span>{funcName}</span>
-				{funcName !== "" && <span class="typ-punct">.</span>}
-				<span class="typ-func">{func.name}</span>
+				<DecoratedFuncName func={func} />
 				<span class="typ-punct">(</span>
 				<span class="arguments pl-4 md:pl-6 flex flex-col">
 					{func.params.map((param, index) => (
