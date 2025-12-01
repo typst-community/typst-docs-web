e269d9c920814177920b38ccf5cad337422b9e02
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2025-12-01 19:51:34 +0900

    test: add all page snapshots

--- tests/e2e/index.test.ts
+++ tests/e2e/index.test.ts
@@ -1,8 +1,20 @@
+import { readFileSync } from "node:fs";
 import { expect, test } from "@playwright/test";
+import type { Page } from "../../src/types/model";
+import { flattenDocs } from "../../src/utils/flattenDocs";
 
-// TODO: Add more e2e tests.
-test("/", async ({ page }) => {
-	await page.goto("/docs/");
+const docsJson = readFileSync("./public/docs.json", "utf-8");
+const docs = JSON.parse(docsJson) as unknown as Page[];
+const [flattenedPages, _] = flattenDocs(docs);
 
-	await expect(page).toHaveScreenshot({ fullPage: true });
+flattenedPages.forEach((page, _) => {
+	const route = page.route;
+	test(`page ${route} should match screenshot`, async ({ page }) => {
+		await page.addInitScript(() => {
+			localStorage.setItem("typst-docs-web-docs-banner-hidden", "true");
+		});
+		await page.setViewportSize({ width: 1280, height: 720 });
+		await page.goto(route);
+		await expect(page).toHaveScreenshot({ fullPage: true });
+	});
 });
